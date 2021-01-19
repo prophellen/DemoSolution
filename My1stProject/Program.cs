976a033eb9ba65aeeb8a35c1ff0d736e3986c334
prophellen <helene.brewitz@gmail.com>
2021-01-19 10:22:19 +0100

    Initial commit

---
+++ My1stProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace My1stProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 10;
+            Console.WriteLine(myNumber);
+        }
+    }
+}
